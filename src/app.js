243466a67ab93160731980fc4c55a63327e5b890
Arthur Oliveira <arthurods37@gmail.com>
2023-04-10 11:30:45 -0400

    adding tweets and post (not working)

--- src/app.js
+++ src/app.js
@@ -6,12 +6,40 @@ application.use(cors({origin: 'http://127.0.0.1:5501'}));
 application.use(express.json());
 
 let user = [];
+let tweets = [];
+let url = '';
 
 application.post('/sign-up', (req, res) =>{
     const {username, avatar} = req.body;
     user.push({username, avatar});
     console.log(user);
-    res.send({message: 'OK'});   
+    url = avatar;
+    res.status(200).send('OK');   
+})
+
+application.get('/tweets', (req, res) =>{
+    if(tweets.length === 0){
+        res.send([]);
+    } else {
+        res.send({tweets});
+    }
+})
+
+application.post('/tweets', (req, res) =>{
+    const {username, text} = req.body;
+    if(user.find((logged) => logged.username !== username)){
+        res.send('UNAUTHORIZED');
+        return;
+    }
+    const single = {
+        username: username,
+        avatar: url,
+        tweet: text
+    }
+    tweets.push(single);
+
+    res.status(200).send('OK')
+
 })
 
 application.listen(5000, () => {
