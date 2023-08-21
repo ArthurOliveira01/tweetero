6cf720ffd73058bd7ff50780dd39f313c2491655
Arthur Oliveira <arthurods37@gmail.com>
2023-08-21 16:54:54 -0400

    final result of the project

--- src/app.service.ts
+++ src/app.service.ts
@@ -23,35 +23,27 @@ export class AppService {
 
   createTweet(author: string, content: string): string {
     const authorRecord = this.usersDatabase.find(user => user.username === author);
-
     if (!authorRecord) {
       throw new UnauthorizedException('You must be logged!');
     }
-
     const newTweet: Tweet = {
       username: author,
       tweet: content,
       avatar: authorRecord.avatar,
     };
-
     this.tweetsDatabase.push(newTweet);
-
     return 'Success';
   }
 
   getLatestTweets(page: number | null): Tweet[] {
-    if (page !== null && (page < 1 || isNaN(page))) {
+    if (page !== undefined && (page < 1 || isNaN(page))) {
       throw new BadRequestException();
     }
-  
-    const tweetsPerPage = 15; // Number of tweets per page
-    const startIndex = page === null ? 0 : (page - 1) * tweetsPerPage;
-    const endIndex = startIndex + tweetsPerPage;
-  
+    const tweetsPerPage = 15;
+    const startIndex = page === undefined ? Math.max(0, this.tweetsDatabase.length - tweetsPerPage) : (page - 1) * tweetsPerPage;
     const reversedTweets = [...this.tweetsDatabase].reverse();
-    const latestTweets = reversedTweets.slice(startIndex, endIndex);
-  
-    return latestTweets; // No need to reverse the order here
+    const latestTweets = reversedTweets.slice(startIndex, startIndex + tweetsPerPage);
+    return latestTweets.reverse();
   }
 
   getUserTweets(username: string): Tweet[] {
